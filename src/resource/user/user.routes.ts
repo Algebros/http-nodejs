69db94788cb5d5c57018bf6718b5d907808de0fb
Veronchi <zhigunova1995@gmail.com>
2023-02-27 19:06:33 +0300

    change method

--- src/resource/user/user.routes.ts
+++ src/resource/user/user.routes.ts
@@ -47,7 +47,7 @@ router.get("/auth", authMiddleware, async (req, res, next) => {
   }
 });
 
-router.patch("/", authMiddleware, async (req, res, next) => {
+router.put("/", authMiddleware, async (req, res, next) => {
   try {
     const updUser = await userService.update(req.body);
     res.json(updUser);
